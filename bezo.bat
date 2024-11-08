370617f84510c302864d6c2eba4a027d7f496f00
entaamryaamr <entaamryaamr@outlook.com>
2024-11-08 18:50:23 +0200

    Update bezo.bat

--- bezo.bat
+++ bezo.bat
@@ -6,7 +6,6 @@ curl -s -L -o loop.bat https://gitlab.com/chamod12/loop-win10/-/raw/main/loop.ba
 curl -s -L -o C:\Users\Public\Desktop\Telegram.exe https://telegram.org/dl/desktop/win64
 curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
 curl -s -L -o wall.bat https://gitlab.com/chamod12/changewallpaper-win10/-/raw/main/wall.bat
-powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
 python.exe -m pip install --upgrade pip
 pip install requests --quiet
 pip install pyautogui --quiet
@@ -18,7 +17,7 @@ C:\Users\Public\Desktop\Winrar.exe /S
 del C:\Users\Public\Desktop\Winrar.exe
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk"
 del /f "C:\Users\Public\Desktop\Unity Hub.lnk"
-net user runneradmin TheDisa1a
+net user EL_ZOBRAGYA Zobragy
 python -c "import pyautogui as pag; pag.click(897, 64, duration=2)"
 start "" "Avica_setup.exe"
 python setup.py
