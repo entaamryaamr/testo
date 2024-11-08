7413040514fbbf9e38061f4208506e4f70129d19
entaamryaamr <entaamryaamr@outlook.com>
2024-11-08 18:37:08 +0200

    Update show.bat

--- show.bat
+++ show.bat
@@ -6,11 +6,6 @@ python -c "import pyautogui as pag; pag.click(155, 554, duration=2)"
 python -c "import pyautogui as pag; pag.click(637, 417, duration=2)"
 python -c "import pyautogui as pag; pag.click(588, 10, duration=2)"
 
-echo  ______ _        __________  ____  _____            _______     __      
-echo |  ____| |      |___  / __ \|  _ \|  __ \     /\   / ____\ \   / //\    
-echo | |__  | |  ______ / / |  | | |_) | |__) |   /  \ | |  __ \ \_/ //  \   
-echo |  __| | | |______/ /| |  | |  _ <|  _  /   / /\ \| | |_ | \   // /\ \  
-echo | |____| |____   / /_| |__| | |_) | | \ \  / ____ \ |__| |  | |/ ____ \ 
-echo |______|______| /_____\____/|____/|_|  \_\/_/    \_\_____|  |_/_/    \_\
-
+echo ************************
+echo EL-ZOBRAGYA    
 echo ************************
