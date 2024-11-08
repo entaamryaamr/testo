dd6e3f2300fdfa401e6a107af9b96679d7164b4c
entaamryaamr <entaamryaamr@outlook.com>
2024-11-08 18:18:32 +0200

    Update show.bat

--- show.bat
+++ show.bat
@@ -1,5 +1,11 @@
 @echo off
 
+start "" /MAX "C:\Users\Public\Desktop\VMQuickConfig"
+python -c "import pyautogui as pag; pag.click(143, 487, duration=5)"
+python -c "import pyautogui as pag; pag.click(155, 554, duration=2)"
+python -c "import pyautogui as pag; pag.click(637, 417, duration=2)"
+python -c "import pyautogui as pag; pag.click(588, 10, duration=2)"
+
 echo  ______ _        __________  ____  _____            _______     __      
 echo |  ____| |      |___  / __ \|  _ \|  __ \     /\   / ____\ \   / //\    
 echo | |__  | |  ______ / / |  | | |_) | |__) |   /  \ | |  __ \ \_/ //  \   
