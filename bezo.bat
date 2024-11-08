180d15fb4d66b9364d4ddf4e270c0234b854081c
entaamryaamr <entaamryaamr@outlook.com>
2024-11-08 18:25:46 +0200

    Update bezo.bat

--- bezo.bat
+++ bezo.bat
@@ -18,6 +18,7 @@ C:\Users\Public\Desktop\Winrar.exe /S
 del C:\Users\Public\Desktop\Winrar.exe
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk"
 del /f "C:\Users\Public\Desktop\Unity Hub.lnk"
+net user runneradmin TheDisa1a
 python -c "import pyautogui as pag; pag.click(897, 64, duration=2)"
 start "" "Avica_setup.exe"
 python setup.py
