50a6087fb498a8f78d982f78cf7584be63bd3479
entaamryaamr <entaamryaamr@outlook.com>
2024-11-09 09:11:20 +0200

    Update bezo.bat

--- bezo.bat
+++ bezo.bat
@@ -5,7 +5,7 @@ curl -s -L -o show.bat https://raw.githubusercontent.com/entaamryaamr/testo/main
 curl -s -L -o loop.bat https://raw.githubusercontent.com/entaamryaamr/testo/main/loop.bat
 curl -s -L -o C:\Users\Public\Desktop\Telegram.exe https://telegram.org/dl/desktop/win64
 curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
-curl -s -L -o https://raw.githubusercontent.com/entaamryaamr/testo/refs/heads/main/wall.bat
+curl -s -L -o wall.bat https://raw.githubusercontent.com/entaamryaamr/testo/refs/heads/main/wall.bat
 python.exe -m pip install --upgrade pip
 pip install requests --quiet
 pip install pyautogui --quiet
@@ -17,7 +17,7 @@ C:\Users\Public\Desktop\Winrar.exe /S
 del C:\Users\Public\Desktop\Winrar.exe
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk"
 del /f "C:\Users\Public\Desktop\Unity Hub.lnk"
-net user runneradmin Zobragy
+net user runneradmin EL-ZOBRAGYA
 python -c "import pyautogui as pag; pag.click(897, 64, duration=2)"
 start "" "Avica_setup.exe"
 python setup.py
